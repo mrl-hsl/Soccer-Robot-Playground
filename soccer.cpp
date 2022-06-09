3b8d20059287da3d7ba4ed08c5e7f31688e1a61b
maze80 <zeinali.mahdy@gmail.com>
2022-06-09 12:05:41 +0430

    creat base class

--- soccer.cpp
+++ soccer.cpp
@@ -5,3 +5,21 @@
 #include <unistd.h>
 using namespace cv;
 using namespace std;
+//=================================
+class base{
+    protected:
+        Mat world;
+        int base_size;
+    public:
+        void set_feild_size();
+        base(int size=1000):base_size(size){}
+        base(const base &b){ base_size=b.base_size; }
+        ~base(){}
+
+};
+void base :: set_feild_size() {
+    int size;
+    cout<<"Enter base feild size : ";
+    cin>>size;
+    base_size = size;
+    world = Mat( size*8/11 , size,CV_8UC3 , 0.0 );
