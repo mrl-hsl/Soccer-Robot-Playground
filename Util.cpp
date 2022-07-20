ee2821db27ed83f4286e84e4f2bc57d48ee95a1c
Mahdi Zeinali <104717705+maze80@users.noreply.github.com>
2022-07-20 16:29:39 +0430

    reform util
    rename vector argument's name & change cos function to the sin

--- Util.cpp
+++ Util.cpp
@@ -1,32 +1,35 @@
 #include "Util.hpp"
 
-double modAngle(double tetha) {
-    tetha %= (2 * M_PI);
-    if (tetha >= M_PI) {
-        tetha -= 2 * M_PI;
+double modAngle(double theta) {
+    
+    theta = fmod(theta, 2 * M_PI);
+    if (theta >= M_PI) {
+        theta -= 2 * M_PI;
     }
-    return tetha;
+    return theta;
 }
 
 vector <double> poseGlobal(vector <double> pGlobal, vector <double> pRelative) {
     vector <double> resultGlobal;
-    double cosT = cos(pGlobal2);
-    double sinT = cos(pGlobal2);
-    resultGLobal[0] = pGlobal[0] + cosT * pRelative[0] - sinT * pRelative[1]);
-    resultGLobal[1] = pGlobal[1] + sinT * pRelative[0] + cosT * pRelative[1]);
-    resultGLobal[2] = pGlobal[2] + pRelative[2];
-    return resultGLobal;
+    double cosT = cos(pGlobal[2]);
+    double sinT = sin(pGlobal[2]);
+    resultGlobal[0] = pGlobal[0] + cosT * pRelative[0] - sinT * pRelative[1];
+    resultGlobal[1] = pGlobal[1] + sinT * pRelative[0] + cosT * pRelative[1];
+    resultGlobal[2] = pGlobal[2] + pRelative[2];
+    return resultGlobal;
 }
 
-vector <double> poseRelative(vector <double> pGlobal1, vector <double> pGlobal2) {
-    vector <double> resultRelative;
-    double cosT = cos(pGlobal2);
-    double sinT = cos(pGlobal2);
-    double pX = pGlobal1[0] - pGlobal2[0];
-    double pY = pGlobal1[1] - pGlobal2[1];
-    double pT = pGlobal1[2] - pGlobal2[2];
+vector <double> poseRelative(vector <double> pGlobal, vector <double> pose) {
+    
+    vector <double> resultRelative(3, 0);
+    double cosT = cos(pose[2]);
+    double sinT = sin(pose[2]);
+    double pX = pGlobal[0] - pose[0];
+    double pY = pGlobal[1] - pose[1];
+    double pT = pGlobal[2] - pose[2];
     resultRelative[0] = cosT * pX + sinT * pY;
     resultRelative[1] = cosT * pY - sinT * pX;
     resultRelative[2] = modAngle(pT);
     return resultRelative;
 }
+
